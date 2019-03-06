910d0c0541a50758a6913a7f466148e26a7880f7
魏巍 <wewin11235@163.com>
2019-03-06 15:08:49 +0800

    feat(learn-python): test

---
+++ test_class.py
@@ -0,0 +1,17 @@
+class Dog():
+    def __init__(self, name, age):
+        """初始化属性 name 和 age"""
+        self.name = name
+        self.age = age
+
+    def sit(self):
+        """模拟小狗蹲下"""
+        print(self.name.title() + " is sitting.")
+
+    def roll_over(self):
+        """模拟小狗打滚"""
+        print(self.name.title() + " rolled over!")
+
+dog = Dog('旺财', 12)
+dog.sit()
+dog.roll_over()
