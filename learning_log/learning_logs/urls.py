316e53d0746f15521df4dbb69ee53fb18306dd89
魏巍 <wewin11235@163.com>
2019-03-10 09:37:14 +0800

    feat(learn-python): demo for learn python

--- learning_log/learning_logs/urls.py
+++ learning_log/learning_logs/urls.py
@@ -1,8 +1,13 @@
 """定义 learning_logs 的 URL 模式"""
 
 from django.conf.urls import url
+
 from . import views
 
 urlpatterns = [
+    # index page
     url(r'^$', views.index, name='index'),
+
+    # 显示所有主题
+    url(r'^topics/$', views.topics, name='topics'),
 ]
