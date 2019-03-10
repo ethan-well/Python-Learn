201bf09d348778f6699c4fb2c4acca1bbce97e90
魏巍 <wewin11235@163.com>
2019-03-10 11:05:51 +0800

    feat(learn-python): demo for learn python

--- learning_log/learning_logs/urls.py
+++ learning_log/learning_logs/urls.py
@@ -9,5 +9,8 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
 
     # 显示所有主题
-    url(r'^topics/$', views.topics, name='topics'),
+    url(r'^topics/?$', views.topics, name='topics'),
+
+    # 特定主题详细页面
+    url(r'^topics/(?P<topic_id>\d+)/$', views.topic, name='topic'),
 ]
