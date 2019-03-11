61e912d240fe86cf5a827e35a10d65fa52f9da44
魏巍 <wewin11235@163.com>
2019-03-12 07:27:34 +0800

    feat(learn-python): demo for learn python

--- learning_log/users/urls.py
+++ learning_log/users/urls.py
@@ -1,10 +1,13 @@
 """为应用程序users定义URL模式"""
+
 from django.conf.urls import url
 
-from django.contrib.auth.views import login
+# from django.contrib.auth.views import login
+from django.contrib.auth import views as auth_views
 
 from . import views
+
 urlpatterns = [
     # 登录页面
-    url(r'^login/$', login, {'template_name': 'users/login.html'}, name='login'),
+    url(r'^login/?$', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
 ]
--- learning_log/users/views.py
+++ learning_log/users/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from . import views
 
 # Create your views here.
+def login(request):
+    return 'xxxx'
+
